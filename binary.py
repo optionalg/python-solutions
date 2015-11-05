a11735bbc057ef6728a13a874fab70d946d647a7
Nhu Nguyen <hoainhu95@gmail.com>
2015-11-05 14:15:54 -0500

    added binary.py

---
+++ binary.py
@@ -0,0 +1,16 @@
+# Implement a binary search tree with insert and remove 
+class Node(Object):
+	def __init__(self, val):
+		self.val = val
+		self.left = None
+		self.right = None
+
+class Tree(Object):
+	def __init__(self):
+		self.root = None
+	#insert and balance tree
+	def insert(self,val):
+		pass
+	#remove and balane tree
+	def remove(self,val):
+		pass
